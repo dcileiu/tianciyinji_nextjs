fe953acfd9416ab938874773eff5e1dad700a00c
tianci <dex_Liu@outlook.com>
2025-07-27 18:22:50 +0800

    feat: update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 /*
  * @Author: tianci tianci1208@outlook.com
  * @Date: 2025-07-18 23:33:28
- * @LastEditors: tianci tianci1208@outlook.com
- * @LastEditTime: 2025-07-26 19:20:30
+ * @LastEditors: tianci dex_Liu@outlook.com
+ * @LastEditTime: 2025-07-27 18:21:53
  * @FilePath: \my-website\src\app\layout.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -48,9 +48,9 @@ export const metadata: Metadata = {
     },
   },
   icons: {
-    icon: "/logo1.svg",
-    shortcut: "/logo1.svg",
-    apple: "/logo1.svg",
+    icon: "/logo.svg",
+    shortcut: "/logo.svg",
+    apple: "/logo.svg",
   },
   manifest: "/manifest.json",
   openGraph: {
